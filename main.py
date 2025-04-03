0d7e7aeaa15dfb1d12bd9d80319374a552843871
pooyaheiran <pooyaheiran@gmail.com>
2025-04-03 23:20:48 +0330

    ytb Downloader

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import youtube_dl
 
 
-url = input("enter: ")
+from __future__ import unicode_literals
+import yt_dlp
 
-print("git test")
+
+url = input("enter video url: ")
+isBan = input("Is YouTube restricted in your country? (y/n)")
+if isBan == "y":
+    proxy = input("enter your ip and port(example: socks5://your-ip:port): ")
+
+ydl_opts = {
+    'proxy': proxy 
+}
+
+
+
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=aCWTzT23UTI&ab_channel=EnjoyChess%D9%84%D8%B0%D8%AA%D8%B4%D8%B7%D8%B1%D9%86%D8%AC'])
